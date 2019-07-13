e4b6b49ec63a472d9bea8e5a1c3531400550590d
David Anders <danders.dev@gmail.com>
2019-07-13 17:31:34 -0500

    add code to set default page 0 and set video mode to 80x25 BW

--- software/generic_test.asm
+++ software/generic_test.asm
@@ -1,9 +1,15 @@
-       org 100h
+       org 003h
 
 section .text
 
 start:
-       MOV     BH,0 ; 0 for DOSBox
+       MOV     AH,5
+       MOV     AL,0
+       INT     10H ; set current video page to 0
+       MOV     AL,2
+       MOV     AH,0 ; set video mode to 80x25 BW
+       INT     10H
+       MOV     BH,0 ; set to current page of 0
        MOV     BL,4
        MOV     CX,1
        MOV     DH,18
